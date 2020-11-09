1d85339c2921df0d2e75fc7f45dba9b3665d51b5
purple910 <2018077362@qq.com>
2020-11-09 14:43:09 +0800

    feat: 搭建注册中心

---
+++ user-eureka/src/test/java/com/example/usereureka/UserEurekaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.usereureka;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class UserEurekaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
